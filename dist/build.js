40cfbf374815b4ee351a7299bb63ae4af62dd27b
Encore <huangrian@gionee.com>
2017-02-27 15:11:32 +0800

    nothing has changed

--- dist/build.js
+++ dist/build.js
@@ -10176,10 +10176,14 @@ module.exports = VueRouter;
 
 
 
+const foo1 = { template: '<div>What\'t doesnt kill you</div>' };
 //2. 定义路由对象routes,用来映射路由信息,每个路由应该映射一个组件.
 //其中"component" 可以是通过 Vue.extend() 创建的组件构造器, 或者，只是一个组件配置对象。 
 //component指该路径映射的组件
 const routes = [{ path: '/foo', name: "foo", component: __WEBPACK_IMPORTED_MODULE_2__component_foo_vue___default.a }, { path: '/bar', name: "bar", component: __WEBPACK_IMPORTED_MODULE_3__component_bar_vue___default.a }];
+
+console.log(foo1);
+console.log(__WEBPACK_IMPORTED_MODULE_2__component_foo_vue___default.a);
 
 // 3. 创建 router 实例，然后传 `routes` 配置  
 const router = new __WEBPACK_IMPORTED_MODULE_1_vue_router___default.a({
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router';
 import Foo from './component/foo.vue';
 import Bar from './component/bar.vue'
 
+const foo1 = { template: '<div>What\'t doesnt kill you</div>'};
 //2. 定义路由对象routes,用来映射路由信息,每个路由应该映射一个组件.
 //其中"component" 可以是通过 Vue.extend() 创建的组件构造器, 或者，只是一个组件配置对象。 
 //component指该路径映射的组件
@@ -11,6 +12,9 @@ const routes = [
 	{path: '/foo', name:"foo", component: Foo},
 	{path: '/bar', name:"bar", component: Bar}
 ]
+
+console.log(foo1);
+console.log(Foo);
 
 // 3. 创建 router 实例，然后传 `routes` 配置  
 const router = new VueRouter({
